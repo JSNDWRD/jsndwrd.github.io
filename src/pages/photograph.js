ec179625c52e5659deee4161c0742a3a1b5f6eb3
Jason Edward <jasonedwardsalim@gmail.com>
2024-03-05 17:56:43 +0700

    daily check

--- src/pages/photograph.js
+++ src/pages/photograph.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Head from "next/head";
 import { FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
-import Skill from "@/components/Skill";
 import Navigation from "@/components/Navigation";
 import Button from "@/components/Button";
 import ProgressBar from "@/components/ProgressBar";
+import "@fontsource-variable/inter";
+import "@fontsource/bebas-neue";
 import "../../node_modules/tailwindcss/tailwind.css";
 import "../app/globals.css";
 
